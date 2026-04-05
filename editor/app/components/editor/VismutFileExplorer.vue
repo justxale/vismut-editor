94de98e663dd922069724040bc2e030440f303ca
justxale <alexev2401@gmail.com>
2026-04-05 23:33:02 +0300

    feat(ui): preparations for console tab

--- editor/app/components/editor/VismutFileExplorer.vue
+++ editor/app/components/editor/VismutFileExplorer.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import ComponentPanel from "~/components/ComponentPanel.vue"
 import FileDisplay from "~/components/editor/displays/FileDisplay.vue"
+import {Tabs, TabsContent, TabsList, TabsTrigger} from "~/components/ui/tabs";
 
 const isVisible = ref<boolean>(true)
 </script>
@@ -8,11 +9,28 @@ const isVisible = ref<boolean>(true)
 <template>
     <div>
         <button @click="isVisible = !isVisible" class="text-text-100">Close</button>
-        <ComponentPanel v-if="isVisible" class="p-4 h-[25vh] p-auto">
-            <div class="grid grid-cols-[repeat(auto-fill,minmax(128px,4fr))] gap-4 overflow-y-auto h-full">
-                <FileDisplay v-for="i in 20" :title="'File' + i.toString()"/>
-            </div>
-        </ComponentPanel>
+        <Tabs default-value="console" v-if="isVisible">
+            <TabsList>
+                <TabsTrigger value="console">
+                    Console
+                </TabsTrigger>
+                <TabsTrigger value="files">
+                    Files
+                </TabsTrigger>
+            </TabsList>
+            <TabsContent value="console">
+                <ComponentPanel class="p-4 h-[25vh] p-auto">
+
+                </ComponentPanel>
+            </TabsContent>
+            <TabsContent value="files">
+                <ComponentPanel class="p-4 h-[25vh] p-auto">
+                    <div class="grid grid-cols-[repeat(auto-fill,minmax(128px,4fr))] gap-4 overflow-y-auto h-full">
+                        <FileDisplay v-for="i in 20" :title="'File' + i.toString()"/>
+                    </div>
+                </ComponentPanel>
+            </TabsContent>
+        </Tabs>
     </div>
 </template>
 
